ffe8feeeab4468e4d194929fe541649231e97189
mlfcjob@163.com <mlfcjob@163.com>
2017-09-14 16:13:48 +0800

    Add first demo of windows platform.
    Signed-off-by: mlfcjob@163.com <mlfcjob@163.com>

---
+++ windows/code/src/testopencv/main.cpp
@@ -0,0 +1,15 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace cv;
+
+int main(int argc, char *argv[])
+{
+	Mat image = imread("1.png", 0);
+	namedWindow("img");
+	imshow("img", image);
+	waitKey(0);             /* Ë¢ÐÂ½çÃæ*/
+
+	return 0;
+}
